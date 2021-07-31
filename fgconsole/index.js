41659a1c8abb9acc72d450368c3a1fe045906ebe
frank-deng <13382084679@163.com>
2021-07-31 23:54:08 +0800

    fgconsole updated

--- fgconsole/index.js
+++ fgconsole/index.js
@@ -2,10 +2,15 @@ const tty = require('tty');
 const FGConsole=require('./fgconsole');
 
 const fgConsole=new FGConsole(process.stdout,()=>{
-    process.exit();
+  process.stdin.setRawMode(false);
+  process.stdin.resume();
+  process.exit();
 },{
-    language:'zh-cn'
+  language:'zh-cn'
 });
 process.stdin.on('data',(data)=>{
-    fgConsole.ondata(data);
+  fgConsole.ondata(data);
 });
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
